e316118e1007ac551e442c4f7ed40ef42ee669ab
stevenmr98 <37566244+stevenmr98@users.noreply.github.com>
2022-05-26 19:38:54 -0600

    nue

---
+++ Models/Data/CustomerDao.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SqlClient;
+
+namespace parking_project.Models.Data
+{
+    public class CustomerDao
+    {
+        private readonly IConfiguration _configuration;
+        string connectionString;
+
+        public CustomerDao(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = _configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public int Insert(Customer customer)
+        {
+            int resultToReturn = 0;//it will save 1 or 0 depending on the result of insertion
+            Exception? exception = new Exception();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+
+
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("InsertCustomer", connection);
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("@Name", customer.Name);
+                    command.Parameters.AddWithValue("@Email", customer.Lastname);
+                    command.Parameters.AddWithValue("@Password", customer.Password);
+                    command.Parameters.AddWithValue("@Email",customer.Email );
+                    command.Parameters.AddWithValue("@Phone", customer.Phone);
+
+                    resultToReturn = command.ExecuteNonQuery();
+                    connection.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+                throw exception;
+            }
+
+
+            return resultToReturn;
+
+        }
+
+
+    }
+}
--- wwwroot/js/Customer.js
+++ wwwroot/js/Customer.js
@@ -10,54 +10,52 @@
          return false;
      });
      
+function Add() {
 
-    
-    });
+    var customer = {
+        name: $('#nameClient').val(),
+        lastName: $('#lastNameClient').val(),
+        password: $('#passwordClient').val(),
+        email: $('#emailClient').val(),
+        phone: parseInt($('#phoneClient').val()),
+    };
 
-
-
-     function Add() {
-
-         var customer = {
-             name: $('#nameClient').val(),
-             lastName: $('#lastNameClient').val(),
-             password: $('#passwordClient').val(),
-             email: $('#emailClient').val(),
-             phone: parseInt($('#phoneClient').val()),
-         };
-
-       
+   
 
 
-         if (customer != null) {
+    if (customer != null) {
 
-             $.ajax({
-                 url: "/Customers/Post",
-                 data: JSON.stringify(customer), //converte la variable estudiante en tipo json
-                 type: "POST",
-                 contentType: "application/json;charset=utf-8",
-                 dataType: "json",
-                 success: function (result) {
-                     $('#nameClient').val('');
-                     $('#lastNameClient').val('');
-                     $('#email').val('');
-                     $('#passwordClient').val('');
-                     $('#phoneClient').val('');
-                     
-                 },
-                 error: function (errorMessage) {
-                     if (errorMessage === "no connection") {
-                         $('#result').text("Error en la conexión.");
-                     }
-                     
-                     $('#passwordClient').val('');
-                 }
-             });
+        $.ajax({
+            url: "/Customer/Insert",
+            data: JSON.stringify(customer), //converte la variable estudiante en tipo json
+            type: "POST",
+            contentType: "application/json;charset=utf-8",
+            dataType: "json",
+            success: function (result) {
 
-         }
+                // alert("resultado: "+result);
+                $('#nameClient').val('');
+                $('#lastNameClient').val('');
+                $('#email').val('');
+                $('#passwordClient').val('');
+                $('#phoneClient').val('');
+            },
+            error: function (errorMessage) {
+                if (errorMessage === "no connection") {
+                    $('#result').text("Error en la conexión.");
+                }
+                $('#nameClient').val('');
+                $('#lastNameClient').val('');
+                $('#email').val('');
+                $('#passwordClient').val('');
+                $('#phoneClient').val('');
+            }
+        });
+
+    }
 
 
-     }
+}
 
     function Validate(user, pass) {
         var record = {
@@ -94,4 +92,3 @@
             }
         });
     }
-
