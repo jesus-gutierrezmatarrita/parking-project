f0dfe998cbfe485f08b17bdd8fd6d777ab528d39
richy0799 <ricarchg@gmail.com>
2022-06-04 23:50:57 -0600

    CRUD Role

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using parking_project.Models.Data;
+using parking_project.Models.Domain;
+
+namespace parking_project.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        RoleDAO roleDao;
+
+        public RoleController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            roleDao = new RoleDAO(_configuration);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Insert([FromBody] Role role)
+        {
+            int resultToReturn = roleDao.Insert(role);
+            return Ok(resultToReturn);
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(roleDao.Get());
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return Ok(roleDao.Delete(id));
+        }
+
+        public IActionResult Update([FromBody] Role role)
+        {
+            return Ok(roleDao.Update(role));
+        }
+
+        public IActionResult GetById(int id)
+        {
+            Role role = roleDao.GetById(id);
+            return Ok(role);
+        }
+    }
+}
---
+++ Models/Domain/Role.cs
@@ -0,0 +1,21 @@
+﻿namespace parking_project.Models.Domain
+{
+    public class Role
+    {
+        private int id;
+        private string name;
+
+        public Role()
+        {
+        }
+
+        public Role(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
